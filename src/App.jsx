3e7ebf79d6425a1a642ae0f6ef4b4096870cc00e
AlexandrShnipov <sshnipov@gmail.com>
2021-10-20 17:31:47 +0300

    added contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from '../src/assets/common/header/Header';
 import Home from '../src/assets/components/pages/home/Home';
 import About from '../src/assets/components/pages/about/About';
 import Education from '../src/assets/components/pages/education/Education';
+import Contact from '../src/assets/components/pages/contact/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
    <Home/>
    <About/>
    <Education/>
+   <Contact/>
     </div>
   );
 }
---
+++ src/assets/components/pages/contact/Contact.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import s from './Contact.module.scss';
+
+const Contact = () => {
+
+    return (
+        <section className={s.section}>
+            <div className={s.sectionContainer}>
+                <div className={s.globalContainer}>
+                    <div className={s.sectionContent}>
+                        <div className={s.addressBlock}>
+                            <h3 className={s.addressBlockTitle}> Contact</h3>
+                            <p className={s.addressBlockText}>You'll called for yielding male, so lights Stars abundantly, is their.</p>
+                            <p className={s.address}>Vitebsk, Belarus</p>
+                            <a className={s.tel} type="tel" href="tel:+375297179741">+375 (29) 717-97-41</a>
+                            <a className={s.email} href="mailto:SShnipov@gmail.com">SShnipov@gmail.com</a>
+                        </div>
+                        <div className={s.formBlock}>
+                            <h4 className={s.formBlockText}>Let's grab a coffee and jump on conversation <span className={s.textColor}>chat with me.</span> </h4>
+                           <form className={s.form} action="/">
+                               <input className={`${s.inputName} ${s.formItem}`} type="text" placeholder="Your Name" title="Please enter your name" required/>
+                               <input className={`${s.inputEmail} ${s.formItem}`} type="email" placeholder="Your Email" title="Please enter email" required/>
+                               <textarea className={`${s.textarea} ${s.formItem}`} name="text" placeholder="Message" required/>
+                               <button className={s.formButton} type="submit">Contact me</button>
+                           </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Contact;
