dd412bf5f7e204d3238056f272c459313c908d0e
AlexandrShnipov <sshnipov@gmail.com>
2021-10-26 23:25:59 +0300

    added arrow caruselProjects

--- src/assets/common/caruselProjects/CaruselProjects.jsx
+++ src/assets/common/caruselProjects/CaruselProjects.jsx
@@ -18,86 +18,83 @@ export default function SimpleSlider() {
     };
 
     return (
-        <div className={s.sliderWrap}>
-            <Slider {...settings} className={"sliderWrap"} dotsClass={"dots"}>
-                <div className={s.slider}>
-                    {/* 1 */}
-                    <div className={s.sliderItems}>
-                        {/* 1.1 */}
-                        <div className={s.sliderItem}>
-                            <div className={s.imgBox}>
-                                <div className={s.imgBoxSquare}>
-                                    <img className={s.imgBoxImg} src={desire} alt="Desire" />
-                                    <div className={s.imgBoxLinks}>
-                                        <Link className={`${s.imgBoxLink} ${s.imgBoxLinkView}`} to="/">
-                                            view project
-                                        </Link>
-                                        <Link className={`${s.imgBoxLink} ${s.imgBoxLinkCode}`} to="/">
-                                            open sourse code
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={s.sliderItemContent}>
-                                <h3 className={s.sliderItemContentTitle}>
-                                    Desire
-                                </h3>
-                                <div className={s.sliderItemContentText}>
-                                    <p className={s.sliderItemContentStack}>
-                                        Stack:
-                                    </p>
-                                    <span>HTML, CSS, SCSS, JS, Gulp.</span>
+        <Slider {...settings} className={"sliderWrap"} dotsClass={"dots"}>
+            <div className={s.slider}>
+                {/* 1 */}
+                <div className={s.sliderItems}>
+                    {/* 1.1 */}
+                    <div className={s.sliderItem}>
+                        <div className={s.imgBox}>
+                            <div className={s.imgBoxSquare}>
+                                <img className={s.imgBoxImg} src={desire} alt="Desire" />
+                                <div className={s.imgBoxLinks}>
+                                    <Link className={`${s.imgBoxLink} ${s.imgBoxLinkView}`} to="/">
+                                        view project
+                                    </Link>
+                                    <Link className={`${s.imgBoxLink} ${s.imgBoxLinkCode}`} to="/">
+                                        open sourse code
+                                    </Link>
                                 </div>
                             </div>
                         </div>
+                        <div className={s.sliderItemContent}>
+                            <h3 className={s.sliderItemContentTitle}>
+                                Desire
+                            </h3>
+                            <div className={s.sliderItemContentText}>
+                                <p className={s.sliderItemContentStack}>
+                                    Stack:
+                                </p>
+                                <span>HTML, CSS, SCSS, JS, Gulp.</span>
+                            </div>
+                        </div>
+                    </div>
 
-                        {/* 1.2 */}
-                        <div className={s.sliderItem}>
-                            <div className={s.imgBox}>
-                                <div className={s.imgBoxSquare}>
-                                    <img className={s.imgBoxImg} src={thrivetalk} alt="thrivetalk" />
-                                    <div className={s.imgBoxLinks}>
-                                        <Link className={`${s.imgBoxLink} ${s.imgBoxLinkView}`} to="/">
-                                            view project
-                                        </Link>
-                                        <Link className={`${s.imgBoxLink} ${s.imgBoxLinkCode}`} to="/">
-                                            open sourse code
-                                        </Link>
-                                    </div>
+                    {/* 1.2 */}
+                    <div className={s.sliderItem}>
+                        <div className={s.imgBox}>
+                            <div className={s.imgBoxSquare}>
+                                <img className={s.imgBoxImg} src={thrivetalk} alt="thrivetalk" />
+                                <div className={s.imgBoxLinks}>
+                                    <Link className={`${s.imgBoxLink} ${s.imgBoxLinkView}`} to="/">
+                                        view project
+                                    </Link>
+                                    <Link className={`${s.imgBoxLink} ${s.imgBoxLinkCode}`} to="/">
+                                        open sourse code
+                                    </Link>
                                 </div>
                             </div>
-                            <div className={s.sliderItemContent}>
-                                <h3 className={s.sliderItemContentTitle}>
-                                    Thrivetalk
-                                </h3>
-                                <div className={s.sliderItemContentText}>
-                                    <p className={s.sliderItemContentStack}>
-                                        Stack:
-                                    </p>
-                                    <span>HTML, CSS, SCSS, Grid, JS, Gulp.
-                                    </span>
-                                </div>
+                        </div>
+                        <div className={s.sliderItemContent}>
+                            <h3 className={s.sliderItemContentTitle}>
+                                Thrivetalk
+                            </h3>
+                            <div className={s.sliderItemContentText}>
+                                <p className={s.sliderItemContentStack}>
+                                    Stack:
+                                </p>
+                                <span>HTML, CSS, SCSS, Grid, JS, Gulp.
+                                </span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
-                </div>
-                <div>
-                    <h3>6</h3>
-                </div>
-            </Slider>
-        </div>
-
+            </div>
+            <div>
+                <h3>2</h3>
+            </div>
+            <div>
+                <h3>3</h3>
+            </div>
+            {/* <div>
+                <h3>4</h3>
+            </div> */}
+            {/* <div>
+                <h3>5</h3>
+            </div> */}
+            {/* <div>
+                <h3>6</h3>
+            </div> */}
+        </Slider>
     );
 }
