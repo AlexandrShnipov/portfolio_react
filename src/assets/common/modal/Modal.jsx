d75490bb75d5eba657ae1b0bb65608de325d1655
AlexandrShnipov <sshnipov@gmail.com>
2021-12-25 19:45:43 +0300

    added modal

---
+++ src/assets/common/modal/Modal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from './Madal.module.scss';
+
+const Modal = () => {
+
+  return (
+    <div className="s.modalBox">
+      <div className="s.modalBoxIsLoading"></div>
+    </div>
+  )
+}
